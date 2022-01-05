845460b929edc835fcb90fc0225ab3bdaa21a639
Heron Eto <41599309+heroneto@users.noreply.github.com>
2022-01-05 17:42:28 -0300

    added github auth with next-auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,15 +1,19 @@
 import { FaGithub }  from 'react-icons/fa'
 import { FiX }  from 'react-icons/fi'
+import { signIn, useSession, signOut } from "next-auth/react"
+
+
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const isUserLoggerIn = true
+  const { status, data } = useSession()
 
-  return isUserLoggerIn ? (
+  return status === 'authenticated' ? (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
       Heron Eto
@@ -19,6 +23,7 @@ export function SignInButton() {
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sign in with Github
