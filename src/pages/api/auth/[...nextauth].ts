a70742be90eb194a1d441ed8a6e4ca2245dc6588
Heron Eto <41599309+heroneto@users.noreply.github.com>
2022-01-06 22:31:32 -0300

    added user save when login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { query as q } from 'faunadb'
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -9,11 +11,44 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       authorization: {
         params: {
-          scope: 'read:user'
-        }
+          scope: 'read:user',          
+        }              
       }
-    }),
-    
+    })
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user.user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+
+    }
+  }
 })
